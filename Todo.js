1a59d3363139a20f4b75462ac83c4351ca595ace
sohail <shaiksohail2332000@gmail.com>
2021-04-13 23:14:11 +0530

    initial commit

---
+++ Todo.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let Todo = new Schema({
+  todo_description: {
+    type: String,
+  },
+  todo_responsible: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("Todo", Todo);
---
+++ index.js
@@ -0,0 +1,97 @@
+var express = require("express");
+var app = express();
+var cors = require("cors");
+var mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+var PORT = 5000;
+var Todo = require("./Todo");
+var todoRouter = express.Router();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect(
+  "mongodb+srv://admin:Sohail@99@cluster0.aaenc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+const connection = mongoose.connection;
+connection.once("open", function () {
+  console.log("connected to mongodb successfully");
+});
+
+todoRouter.route("/").get(function (req, res) {
+  Todo.find(function (err, todos) {
+    if (err) {
+      console.log("error" + err);
+    } else {
+      res.json(todos);
+    }
+  });
+});
+
+todoRouter.route("/:id").get(function (req, res) {
+  Todo.findById(req.params.id)
+    .then((todo) => {
+      if (!todo) {
+        res.statusCode(404).send("No todo found");
+      } else {
+        res.json(todo);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+todoRouter.route("/add").post(function (req, res) {
+  let todo = new Todo(req.body);
+  todo
+    .save()
+    .then((todo) => {
+      res.status(200).json({ todo });
+      //   console.log("successfully added");
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+      //   console.log(err);
+    });
+});
+todoRouter.route("/del/:id").delete((req, res) => {
+  // res.status(200).send(req.params.id);
+  Todo.findByIdAndRemove(req.params.id)
+    .then((todo) => {
+      if (!todo) {
+        return res.status(401).send("something went wrong");
+      } else {
+        res.status(200).send("Delete success");
+      }
+    })
+    .catch((err) => res.status(401).send(err));
+});
+todoRouter.route("/edit/:id").post(function (req, res) {
+  var id = req.params.id;
+  Todo.findById(id, (err, todo) => {
+    if (!todo) res.status(404).send("object with givcen id not found");
+    else {
+      todo.todo_description = req.body.todo_description;
+      todo.todo_responsible = req.body.todo_responsible;
+
+      todo
+        .save()
+        .then((todo) => {
+          res.status(200).send("update success");
+        })
+        .catch((err) => {
+          res.status(400).send("something went wrong please try again");
+        });
+    }
+  });
+});
+
+app.use("/todo", todoRouter);
+
+app.listen(PORT, function () {
+  console.log("listening to port " + PORT);
+});
